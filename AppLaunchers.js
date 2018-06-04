c1cdede70ac8d5951e0d917b19f8c6ad78b9e908
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-06-04 12:24:42 -0700

    feat: add logos to app launchers

--- AppLaunchers.js
+++ AppLaunchers.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, View } from "react-360";
+import { Text, View, Image, asset, NativeModules } from "react-360";
 import styled from "styled-components";
 
 const FlexBox = styled(View)`
@@ -27,30 +27,71 @@ const AppLauncher = styled(View)`
   border-width: 2px;
 `;
 
+const Logo = styled(Image)`
+  height: 100%;
+  max-height: 75%;
+`;
+
 const Title = styled(Text)`
   font-size: 30px;
+  text-align: center;
 `;
 
 export default class AppLaunchers extends Component {
+  gotoApp = url => () => {
+    NativeModules.LinkingManager.openURL(url);
+  };
   render() {
     return (
       <FlexBox>
-        <AppLauncher>
+        <AppLauncher
+          onInput={this.gotoApp(
+            "https://www.ellucian.com/student-information-system/"
+          )}
+          pointerEvents="box-only"
+        >
+          <Logo source={asset("banner-by-ellucian.png")} />
           <Title>Banner</Title>
         </AppLauncher>
-        <AppLauncher>
+        <AppLauncher
+          onInput={this.gotoApp("https://www.canvaslms.com/")}
+          pointerEvents="box-only"
+        >
+          <Logo source={asset("canvas-by-instructure.png")} />
           <Title>Canvas</Title>
         </AppLauncher>
-        <AppLauncher>
-          <Title>Peoplesoft</Title>
+        <AppLauncher
+          onInput={this.gotoApp("http://www.blackboard.com")}
+          pointerEvents="box-only"
+        >
+          <Logo source={asset("blackboard.png")} />
+          <Title>BlackBoard</Title>
         </AppLauncher>
-        <AppLauncher>
+        <AppLauncher
+          onInput={this.gotoApp("https://en.wikipedia.org/wiki/University")}
+          pointerEvents="box-only"
+        >
+          <Logo
+            source={asset("learning-services.png")}
+            style={{ backgroundColor: "white" }}
+          />
           <Title>Learning Center</Title>
         </AppLauncher>
-        <AppLauncher>
+        <AppLauncher
+          onInput={this.gotoApp("https://en.wikipedia.org/wiki/University")}
+          pointerEvents="box-only"
+        >
+          <Logo
+            source={asset("university.png")}
+            style={{ backgroundColor: "white" }}
+          />
           <Title>Campus Services</Title>
         </AppLauncher>
-        <AppLauncher>
+        <AppLauncher
+          onInput={this.gotoApp("https://www.oculus.com")}
+          pointerEvents="box-only"
+        >
+          <Logo source={asset("oculus.png")} />
           <Title>Oculus</Title>
         </AppLauncher>
       </FlexBox>
